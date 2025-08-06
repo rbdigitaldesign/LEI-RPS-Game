a6e516684c30ce2f9fd2082c2503ef9e31cd5477
Richard Bartlett <rbdigitaldesign11@gmail.com>
2025-08-06 06:15:19 +0000

    The shufflearray function is still present it seems. Double check the co

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,11 +2,9 @@
 import type { Pod, Move } from './types';
 
 export const PODS: Omit<Pod, 'id'>[] = [
-  { name: 'Pandas', manager: 'Leila', emoji: '🐼' },
   { name: 'Owls', manager: 'Rebecca', emoji: '🦉' },
   { name: 'Racoons', manager: 'Media team', emoji: '🦝' },
   { name: 'Octopus', manager: 'Paul', emoji: '🐙' },
-  { name: 'Platypus', manager: 'Simon', emoji: '🦫' },
   { name: 'Dolphins', manager: 'Jamie', emoji: '🐬' },
   { name: 'Orcas', manager: 'Tim', emoji: '🐳' },
   { name: 'Rakalis', manager: 'Richard', emoji: '🐀' },
@@ -16,6 +14,8 @@ export const PODS: Omit<Pod, 'id'>[] = [
   { name: 'Senior Staff', manager: 'Leadership', emoji: '👑' },
   { name: 'Associate Directors', manager: 'Management', emoji: '🧑‍💼' },
   { name: 'Portfolio Managers', manager: 'Strategy', emoji: '📈' },
+  { name: 'Pandas', manager: 'Leila', emoji: '🐼' },
+  { name: 'Platypus', manager: 'Simon', emoji: '🦫' },
 ];
 
 export const MOVES: Move[] = ['rock', 'paper', 'scissors'];
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,13 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export const shuffleArray = <T,>(array: T[]): T[] => {
-  if (typeof window === "undefined") {
-    // Return original array if not in browser environment
-    return array;
-  }
-  return [...array].sort(() => Math.random() - 0.5);
-};
-
-    
