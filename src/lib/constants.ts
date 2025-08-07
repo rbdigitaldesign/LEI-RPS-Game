8d879068c06a2a900261d2f00062d6898a2b7e48
Richard Bartlett <rbdigitaldesign11@gmail.com>
2025-08-07 02:19:20 +0000

    Update Manager of Bees to Victoria and manager of Racoons to Aaron

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,14 +3,14 @@ import type { Pod, Move } from './types';
 
 export const PODS: Omit<Pod, 'id'>[] = [
   { name: 'Owls', manager: 'Rebecca', emoji: '🦉' },
-  { name: 'Racoons', manager: 'Media team', emoji: '🦝' },
+  { name: 'Racoons', manager: 'Aaron', emoji: '🦝' },
   { name: 'Octopus', manager: 'Paul', emoji: '🐙' },
   { name: 'Dolphins', manager: 'Jamie', emoji: '🐬' },
   { name: 'Orcas', manager: 'Tim', emoji: '🐳' },
   { name: 'Rakalis', manager: 'Richard', emoji: '🐀' },
   { name: 'Capybaras', manager: 'Laura', emoji: '🐹' },
   { name: 'Wombats', manager: 'Carina', emoji: '🐨' },
-  { name: 'Bees', manager: 'Evan', emoji: '🐝' },
+  { name: 'Bees', manager: 'Victoria', emoji: '🐝' },
   { name: 'Platypus', manager: 'Simon', emoji: '🦫' },
   { name: 'Senior Staff', manager: 'Mark C and Andrew B.', emoji: '👑' },
   { name: 'Associate Directors', manager: 'Maddie M and Sharon S', emoji: '🧑‍💼' },
