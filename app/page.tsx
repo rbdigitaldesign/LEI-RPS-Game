4697cf6603049ad18e3bc28d8f8226a1a3930b17
Richard Bartlett <rbdigitaldesign11@gmail.com>
2025-08-12 04:00:06 +0000

    Now there's a 404 and the page could not be found. Go back a few steps a

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+'use client';
+
+import { Suspense } from 'react';
+import { MainPageContent } from '@/components/main-page';
+
+export default function Home() {
+  return (
+    <Suspense>
+      <MainPageContent />
+    </Suspense>
+  );
+}
