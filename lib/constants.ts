57eeb81906ebb703998d8359ad6d9bad20f4fd81
Richard Bartlett <rbdigitaldesign11@gmail.com>
2025-08-12 06:18:50 +0000

    Revert some code back as the App is now  404 errors again

---
+++ lib/constants.ts
@@ -0,0 +1,24 @@
+
+import type { Pod, Move } from './types';
+
+export const PODS: Omit<Pod, 'id'>[] = [
+  { name: 'Owls', manager: 'Rebecca', emoji: '🦉' },
+  { name: 'Racoons', manager: 'Aaron and Dave', emoji: '🦝' },
+  { name: 'Octopus', manager: 'Paul', emoji: '🐙' },
+  { name: 'Dolphins', manager: 'Jamie', emoji: '🐬' },
+  { name: 'Orcas', manager: 'Tim', emoji: '🐳' },
+  { name: 'Rakalis', manager: 'Richard', emoji: '🐀' },
+  { name: 'Capybaras', manager: 'Laura', emoji: '🐹' },
+  { name: 'Wombats', manager: 'Carina', emoji: '🐨' },
+  { name: 'Bees', manager: 'Victoria', emoji: '🐝' },
+  { name: 'Platypus', manager: 'Simon', emoji: '🦫' },
+  { name: 'Functional Leads', manager: 'Mark C and Andrew B.', emoji: '👑' },
+  { name: 'Associate Directors', manager: 'Maddie M and Sharon S', emoji: '🧑‍💼' },
+  { name: 'Portfolio Managers', manager: 'Alex F and Shaun McC', emoji: '📈' },
+  { name: 'Pandas', manager: 'Leila', emoji: '🐼' },
+  { name: 'Travis Cox', manager: 'Travis', emoji: '👨‍🦰' },
+];
+
+export const MOVES: Move[] = ['rock', 'paper', 'scissors'];
+
+    
