f4ab103c1da50d029d072eb6daaa2f68a25674c4
Richard Bartlett <rbdigitaldesign11@gmail.com>
2025-08-12 04:10:34 +0000

    Do need analysis as to why 404 error is occuring

--- src/app/api/[[...genkit]]/route.ts
+++ src/app/api/[[...genkit]]/route.ts
@@ -1,4 +1,3 @@
+'use server';
 import { appRoute } from '@genkit-ai/next';
-import {getGcpCredentials} from 'common-server';
-import {google} from 'googleapis';
 export const { GET, POST } = appRoute();
