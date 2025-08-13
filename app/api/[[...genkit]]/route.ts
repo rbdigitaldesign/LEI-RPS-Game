02c687607c0f68a6e3fb6380ff60c41081241604
Richard Bartlett <rbdigitaldesign11@gmail.com>
2025-08-13 23:04:28 +0000

    Act as principal SRE for a Next.js (app router) app on Firebase App Host

--- app/api/[[...genkit]]/route.ts
+++ app/api/[[...genkit]]/route.ts
@@ -1,3 +1,5 @@
-'use server';
+
 import { appRoute } from '@genkit-ai/next';
+import {getGcpCredentials} from 'common-server';
+import {google} from 'googleapis';
 export const { GET, POST } = appRoute();
