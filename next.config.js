ea23df26bbcda44f280d804aebbe2e4fa9cf2b1a
Richard Bartlett <rbdigitaldesign11@gmail.com>
2025-08-14 02:33:26 +0000

    act as principal sre for rps-pod-showdown-v6. do not touch v5. keep auto

--- next.config.js
+++ next.config.js
@@ -3,11 +3,5 @@ const nextConfig = {
   output: 'standalone',
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
-      { protocol: 'https', hostname: 'i.imgur.com', pathname: '/**' }
-    ]
-  }
 };
 module.exports = nextConfig;
